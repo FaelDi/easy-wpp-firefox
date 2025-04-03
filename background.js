2a43e14434d7228a56bae03238bb07caba25f6f3
FaelDi <faeldii@icloud.com>
2025-04-03 20:58:15 -0300

    adding support to firefox

--- background.js
+++ background.js
@@ -1,13 +1,17 @@
-chrome.contextMenus.create({
+const isFirefox = typeof browser !== 'undefined';
+const contextMenus = isFirefox ? browser.contextMenus : chrome.contextMenus;
+const tabs = isFirefox ? browser.tabs : chrome.tabs;
+
+contextMenus.create({
     id: 'easy-wpp',
     title: 'Iniciar conversa com este número',
     contexts: ['selection']
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
+contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === 'easy-wpp' && info.selectionText) {
         const numero = info.selectionText.replace(/\D/g, ''); // Remove não números
         const url = `https://wa.me/${numero}`;
-        chrome.tabs.create({ url }); // Abre a aba com o link
+        tabs.create({ url }); // Abre a aba com o link
     }
 });
